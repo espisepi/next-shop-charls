ec86d0d117fdedaf87b69eb2693738e37dbacf61
espisepi <sepinaco@gmail.com>
2021-05-06 22:53:23 +0200

    get stripe products with getServerSideProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,10 @@
 import useStore from '@/helpers/store'
 import dynamic from 'next/dynamic'
 
+import { loadStripe } from "@stripe/stripe-js";
+import Stripe from "stripe";
+// import { createCheckoutSession } from "next-stripe/client";
+
 const Scene = dynamic(() => import('@/scenes/Scene'), {
   ssr: false,
 })
@@ -16,10 +20,19 @@ const Page = ({ title }) => {
 
 export default Page
 
-export async function getStaticProps() {
-  return {
-    props: {
-      title: 'Index',
-    },
-  }
+export async function getServerSideProps() {
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2020-08-27",
+  });
+
+  const prices = await stripe.prices.list({
+    active: true,
+    limit: 10,
+    expand: ["data.product"],
+  });
+  console.log(prices);
+
+  return { props: { prices: prices.data, title: 'Index' } };
+
 }
