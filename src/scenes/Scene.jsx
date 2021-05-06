f18ab883e620ba989487e78d1aa505e74a93b1e6
espisepi <sepinaco@gmail.com>
2021-05-06 20:57:55 +0200

    model gtlf added into scene

--- src/scenes/Scene.jsx
+++ src/scenes/Scene.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import * as THREE from 'three';
+import { useGLTF, OrbitControls } from '@react-three/drei';
+
 import Loading from '@/components/canvas/Loading';
+
+function ModelScene() {
+    const gltf = useGLTF('obj/scene.glb');
+    return <primitive object={gltf.scene} />;
+}
 
 export default function Scene() {
     return (
         <>
         <ambientLight />
-        <Loading />
+        <Suspense fallback={<Loading />}>
+            <ModelScene />
+        </Suspense>
+        <OrbitControls />
         </>
     );
 }
